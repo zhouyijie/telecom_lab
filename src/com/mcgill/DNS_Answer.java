6ecbe4688a40472bae478e2f5e33f8f580f56ec9
Ege <ege.ozer@mail.mcgill.ca>
2018-10-15 19:43:34 -0400

    Started Answer section

--- src/com/mcgill/DNS_Answer.java
+++ src/com/mcgill/DNS_Answer.java
@@ -1,5 +1,16 @@
 package com.mcgill;
 
+import java.util.Arrays;
+
 public class DNS_Answer {
+
+    public DNS_Answer() {
+
+    }
+
+    public void answer(byte[] recievedData) {
+        System.out.println(Arrays.toString(recievedData));
+
+    }
 
 }
