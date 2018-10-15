ca7a8419ee483258260b129eff67f7512d1c0a73
Ege <ege.ozer@mail.mcgill.ca>
2018-10-15 19:51:04 -0400

    fixed spacies issues

--- src/com/mcgill/DNS_PacketHeaders.java
+++ src/com/mcgill/DNS_PacketHeaders.java
@@ -1,158 +1,146 @@
 package com.mcgill;
 
 public class DNS_PacketHeaders {
-	// private variables
+    // private variables
 
-	private short ID;
+    private short ID;
 
-	private byte QR;
+    private byte QR;
 
-	private byte OPCODE;
+    private byte OPCODE;
 
-	private byte AA;
+    private byte AA;
 
-	private byte TC, RD, RA, Z, RCODE;
+    private byte TC, RD, RA, Z, RCODE;
 
-	private short QDCOUNT;
+    private short QDCOUNT;
 
-	private short ANCOUNT;
+    private short ANCOUNT;
 
-	private short NSCOUNT;
+    private short NSCOUNT;
 
-	private short ARCOUNT;
-	private byte[] packetHeader;
+    private short ARCOUNT;
+    private byte[] packetHeader;
 
-	public DNS_PacketHeaders(short id, byte qr, byte opCode, byte aa, byte tc, byte rd, byte ra, byte z, byte rCode,
-			short qdCount, short anCount, short nsCount, short arCount) {
-		ID = id;
-		QR = qr;
-		OPCODE = opCode;
-		AA = aa;
-		TC = tc;
-		RD = rd;
-		RA = ra;
-		Z = z;
-		RCODE = rCode;
-		QDCOUNT = qdCount;
-		ANCOUNT = anCount;
-		ARCOUNT = arCount;
-		NSCOUNT = nsCount;
-		byte[] packetHeader = new byte[12];
-		// big endian for id address
-		packetHeader[0] = (byte) (ID >>> 8);
-		packetHeader[1] = (byte) (ID);
-		packetHeader[2] = (byte) ((QR << 7) | (OPCODE << 3) | (AA << 2) | (TC << 1) | RD);
-		packetHeader[3] = (byte) ((RA << 7) | (Z << 4) | RCODE);
-		packetHeader[4] = (byte) (QDCOUNT >>> 8);
-		packetHeader[5] = (byte) QDCOUNT;
-		packetHeader[6] = (byte) (ARCOUNT >>> 8);
-		packetHeader[7] = (byte) ARCOUNT;
-		packetHeader[8] = (byte) (ANCOUNT >>> 8);
-		packetHeader[9] = (byte) ANCOUNT;
-		packetHeader[10] = (byte) (NSCOUNT >>> 8);
-		packetHeader[11] = (byte) NSCOUNT;
-		this.packetHeader = packetHeader;
+    public DNS_PacketHeaders(short id, byte qr, byte opCode, byte aa, byte tc, byte rd, byte ra, byte z, byte rCode,
+                             short qdCount, short anCount, short nsCount, short arCount) {
+        ID = id;
+        QR = qr;
+        OPCODE = opCode;
+        AA = aa;
+        TC = tc;
+        RD = rd;
+        RA = ra;
+        Z = z;
+        RCODE = rCode;
+        QDCOUNT = qdCount;
+        ANCOUNT = anCount;
+        ARCOUNT = arCount;
+        NSCOUNT = nsCount;
+        byte[] packetHeader = new byte[12];
+        // big endian for id address
+        packetHeader[0] = (byte) (ID >>> 8);
+        packetHeader[1] = (byte) (ID);
+        packetHeader[2] = (byte) ((QR << 7) | (OPCODE << 3) | (AA << 2) | (TC << 1) | RD);
+        packetHeader[3] = (byte) ((RA << 7) | (Z << 4) | RCODE);
+        packetHeader[4] = (byte) (QDCOUNT >>> 8);
+        packetHeader[5] = (byte) QDCOUNT;
+        packetHeader[6] = (byte) (ARCOUNT >>> 8);
+        packetHeader[7] = (byte) ARCOUNT;
+        packetHeader[8] = (byte) (ANCOUNT >>> 8);
+        packetHeader[9] = (byte) ANCOUNT;
+        packetHeader[10] = (byte) (NSCOUNT >>> 8);
+        packetHeader[11] = (byte) NSCOUNT;
+        this.packetHeader = packetHeader;
 
-	}
+    }
 
-	public void readHeader(byte[] header) throws Exception {
-		
-		//System.out.println("read headers");
-		this.ID = (short) ((header[0] << 8) | header[1]);
+    public void readHeader(byte[] header) throws Exception {
 
-		String flag1 = byteToBin(header[2]);
-		String flag2 = byteToBin(header[3]);
+        //System.out.println("read headers");
+        this.ID = (short) ((header[0] << 8) | header[1]);
 
-		this.QR = binToByte(flag1.substring(0, 1));
+        String flag1 = byteToBin(header[2]);
+        String flag2 = byteToBin(header[3]);
 
-		this.OPCODE = binToByte(flag1.substring(1, 5));
+        this.QR = binToByte(flag1.substring(0, 1));
 
-		this.AA = binToByte(flag1.substring(5, 6));
+        this.OPCODE = binToByte(flag1.substring(1, 5));
 
-		this.TC = binToByte(flag1.substring(6, 7));
+        this.AA = binToByte(flag1.substring(5, 6));
 
-		this.RD = binToByte(flag1.substring(7, 8));
+        this.TC = binToByte(flag1.substring(6, 7));
 
-		this.RA = binToByte(flag2.substring(0, 1));
+        this.RD = binToByte(flag1.substring(7, 8));
 
-		this.Z = binToByte(flag2.substring(1, 4));
+        this.RA = binToByte(flag2.substring(0, 1));
 
-		this.RCODE = binToByte(flag2.substring(4, 8));
+        this.Z = binToByte(flag2.substring(1, 4));
 
-		this.QDCOUNT = (short) ((header[4] << 8) | header[5]);
+        this.RCODE = binToByte(flag2.substring(4, 8));
 
-		this.ANCOUNT = (short) ((header[6] << 8) | header[7]);
+        this.QDCOUNT = (short) ((header[4] << 8) | header[5]);
 
-		this.NSCOUNT = (short) ((header[8] << 8) | header[9]);
+        this.ANCOUNT = (short) ((header[6] << 8) | header[7]);
 
-		this.ARCOUNT = (short) ((header[10] << 8) | header[11]);
+        this.NSCOUNT = (short) ((header[8] << 8) | header[9]);
 
-		if (RA == 1) {
+        this.ARCOUNT = (short) ((header[10] << 8) | header[11]);
 
-			System.out.println("server does not support recursive queries.");
-		}
+        if (RA == 1) {
 
-		switch (RCODE) {
+            System.out.println("server does not support recursive queries.");
+        }
 
-		case 0:
-			break;// zero error condition
+        switch (RCODE) {
 
-		case 1:
-			System.out.println("format error: the name server was unable to interpret the query");
+            case 0:
+                break;// zero error condition
 
-		case 2:
+            case 1:
+                System.out.println("format error: the name server was unable to interpret the query");
 
-			System.out.println("failure: the name server was unable to process this query due to a problem with the name server");
+            case 2:
+                System.out.println("failure: the name server was unable to process this query due to a problem with the name server");
 
-		case 3:
+            case 3:
+                System.out.println("name error: domain name referenced in the query does not exist");
 
-			System.out.println("name error: domain name referenced in the query does not exist");
+            case 4:
+                System.out.println("not implemented: the name server does not support the requested kind of query");
 
-		case 4:
+            case 5:
+                System.out.println("refused: The name server refuses to perform the requested operation for policy reasons");
+        }
+    }
 
-			System.out.println("not implemented: the name server does not support the requested kind of query");
+    private byte binToByte(String substring) {
+        byte intOut = (byte) Integer.parseInt(substring, 2);
 
-		case 5:
+        return intOut;
+    }
 
-			System.out.println("refused: The name server refuses to perform the requested operation for policy reasons");
+    public byte[] getHeader() {
+        return packetHeader;
+    }
 
-		}
-	}
+    public static String byteToBin(byte byteIn) {
 
-	private byte binToByte(String substring) {
-		byte intOut = (byte) Integer.parseInt(substring, 2);
+        String binOut = "";
 
-		return intOut;
+        if (byteIn < 0) {
 
-	}
+            int intIn = byteIn;
 
-	public byte[] getHeader() {
-		return packetHeader;
+            intIn = intIn + 256;
+            binOut = String.format("%8s", Integer.toBinaryString(intIn & 0xFF)).replace(' ', '0');
 
-	}
+        } else {
+            binOut = String.format("%8s", Integer.toBinaryString(byteIn & 0xFF)).replace(' ', '0');
+        }
 
-	public static String byteToBin(byte byteIn) {
+        return binOut;
 
-		String binOut = "";
-
-		if (byteIn < 0) {
-
-			int intIn = byteIn;
-
-			intIn = intIn + 256;
-
-			binOut = String.format("%8s", Integer.toBinaryString(intIn & 0xFF)).replace(' ', '0');
-
-		}
-
-		else {
-
-			binOut = String.format("%8s", Integer.toBinaryString(byteIn & 0xFF)).replace(' ', '0');
-
-		}
-
-		return binOut;
-
-	}
+    }
 
 }
