05b9a02135904e44e1c22182b035ca900cf2b7c0
Ege <ege.ozer@mail.mcgill.ca>
2018-10-15 14:50:12 -0400

    Parsing the address into bytes

--- src/com/mcgill/DNS_Question.java
+++ src/com/mcgill/DNS_Question.java
@@ -2,4 +2,31 @@ package com.mcgill;
 
 public class DNS_Question {
 
+    private String address;
+    private String qType;
+    private String qClass;
+    byte[] dataSent = new byte[1024];
+    int count = 0;
+
+    public DNS_Question(String address, String qType, String qClass) {
+        this.address = address;
+        this.qType = qType;
+        this.qClass = qClass;
+        parseQName();
+    }
+
+    public void parseQName() {
+
+
+        String s[] = address.split("\\.");
+
+        for (int i = 0; i < s.length; i++) {
+            dataSent[count++] = ((byte) s[i].length());
+
+            for (int j = 0; j < s[i].length(); j++) {
+                dataSent[count++] = (byte) s[i].charAt(j);
+            }
+        }
+        dataSent[count++] = (byte) 0;
+    }
 }
