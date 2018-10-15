c224b0c0d55b45bf90d6e9491612e4548e71ffa5
Ege <ege.ozer@mail.mcgill.ca>
2018-10-15 15:47:19 -0400

    All the optional parameters are captured

--- src/com/mcgill/DnsClient.java
+++ src/com/mcgill/DnsClient.java
@@ -8,11 +8,15 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class DnsClient {
-    private static int port;
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         String argIp = "";
         String argAddress = "";
+        String argType = "A";
+        int argPort = 53;
+        int argMaxR = 3;
+        int argTimeout = 5;
+
         int splittedIntIp[] = new int[4];
         byte[] address = new byte[4];
         //short ID = new byte[2];
@@ -21,9 +25,25 @@ public class DnsClient {
             if (args[i].charAt(0) == '@') {
                 argIp = args[i];
                 argIp = argIp.substring(1);
-                argAddress = args[i+1];
+                argAddress = args[i + 1];
+            } else if (args[i].charAt(0) == '-') {
+                if (args[i].charAt(1) == 't') {
+                    argTimeout = Integer.parseInt(args[i + 1]);
+
+                } else if (args[i].charAt(1) == 'r') {
+                    argMaxR = Integer.parseInt(args[i + 1]);
+
+                } else if (args[i].charAt(1) == 'p') {
+                    argPort = Integer.parseInt(args[i + 1]);
+                } else if (args[i].charAt(1) == 'm') {
+                    argType = "MX";
+                } else if (args[i].charAt(1) == 'n') {
+                    argType = "NS";
+                }
+
             }
         }
+        System.out.println("t:" + argType);
 
         String splittedStringIp[] = argIp.split("\\.");
         if (splittedStringIp.length != 4) {
@@ -42,11 +62,11 @@ public class DnsClient {
 
         Random randomID = new Random(Short.MAX_VALUE + 1);
         //random.nextBytes(ID);
-        
-        
-        DNS_PacketHeaders dnsHeader = new DNS_PacketHeaders((short)randomID.nextInt(), (byte)0, (byte)0, (byte)0, (byte)0, (byte)0, (byte)0, (byte)0, (byte)0, (short)0, (short)0, (short)0, (short)0);
 
-        System.out.println("sending request for "  +argAddress+ "\n"
+
+        DNS_PacketHeaders dnsHeader = new DNS_PacketHeaders((short) randomID.nextInt(), (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (short) 0, (short) 0, (short) 0, (short) 0);
+
+        System.out.println("sending request for " + argAddress + "\n"
                 + "Server: " + Arrays.toString(splittedStringIp));
         
         /*
@@ -56,34 +76,32 @@ public class DnsClient {
 
         DatagramSocket socket = new DatagramSocket();
 
-        
-		byte[] sendData = new byte[] {1};
-		DatagramPacket dnsReqPacket = new DatagramPacket(sendData ,sendData.length,server,port);
+
+        byte[] sendData = new byte[]{1};
+        DatagramPacket dnsReqPacket = new DatagramPacket(sendData, sendData.length, server, argPort);
 
         long startTime = System.currentTimeMillis();
 
-        socket.send(dnsReqPacket);
-        
+//        socket.send(dnsReqPacket);
+
         /*
          * waiting for dns response
-         * 
+         *
          */
-        
 
-        byte[] receiveData = new byte[] {1};
-		DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
+
+        byte[] receiveData = new byte[]{1};
+        DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
 
         socket.receive(packet);
 
-        long endTime   = System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
 
         long totalTime = endTime - startTime;
 
         System.out.println("");
 
         System.out.println("Received data: " + Arrays.toString(receiveData));
-        
-        
 
 
     }
